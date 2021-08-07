ee37229772cc055486f813a16f17492534a1be6f
Md.Mehedi Hasan <61658463+AyonIslam12@users.noreply.github.com>
2021-08-07 13:00:17 +0600

    initilize the project

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div v-if="check_auth">
+      <div id="nav">
+        <router-link :to="{path: 'admin'}">admin</router-link>| 
+        <router-link :to="{path: 'student'}">student</router-link>| 
+        <router-link :to="{path: 'management'}">management</router-link>
+      </div>
+      <h1>auth part</h1>
+    </div>
+    <div v-else>
+      <div id="nav">
+        <router-link :to="{path: 'login'}">login</router-link>| 
+        <router-link :to="{path: 'signup'}">signup</router-link>| 
+        <router-link :to="{path: 'protfolio'}">protfolio</router-link>
+      </div>
+      <h1>not auth part</h1>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return{
+      check_auth: true,
+    }
+  },
+
+  created: function(){
+    if(this.check_auth){
+      this.$router.replace({name:'dashboard'});
+    }else{
+      this.$router.replace({name:'login'});
+    }
+     
+  }
+
+};
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,59 @@
+import { createRouter, createWebHistory } from 'vue-router'
+//backend page
+import Dashboard from '../views/backend/dashboard';
+import admin from '../views/backend/admin';
+import student from '../views/backend/student';
+import management from '../views/backend/management';
+//frontend page
+import Login from '../views/frontend/login';
+import Signup from '../views/frontend/signup';
+import Protfolio from '../views/frontend/protfolio';
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: admin,
+  },
+  {
+    path: '/student',
+    name: 'student',
+    component: student,
+  },
+  {
+    path: '/management',
+    name: 'management',
+    component: management,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+   
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: Signup,
+   
+  },
+  {
+    path: '/protfolio',
+    name: 'protfolio',
+    component: Protfolio,
+   
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
